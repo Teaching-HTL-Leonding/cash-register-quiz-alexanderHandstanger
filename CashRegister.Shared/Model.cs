c68f9a5de816e969e34c0ecba552df891fb38e4d
alexanderHandstanger <zombeyfan.ah@gmail.com>
2021-05-05 19:25:58 +0200

    "Create Web APIs" done

--- CashRegister.Shared/Model.cs
+++ CashRegister.Shared/Model.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace CashRegister.Shared
 {
+    public class ReceiptLineDto
+    {
+        public int ProductID { get; set; }
+        public int Amount { get; set; }
+    }
     public class Product
     {
         [JsonPropertyName("id")]
